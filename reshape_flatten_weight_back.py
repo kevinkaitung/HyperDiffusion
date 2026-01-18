01f73cfb6b318080482184c2eb4dfebefed269cf
Kai-Chen Tung <dadado0570@gmail.com>
2026-01-17 17:56:11 -0800

    Tweak

--- reshape_flatten_weight_back.py
+++ reshape_flatten_weight_back.py
@@ -14,15 +14,16 @@ def reshape_weight_back(input, example_network):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--flatten_weight_path", type=str)
-    parser.add_argument("--sample_model_path", type=str)
-    parser.add_argument("--save_path", type=str)
+    parser.add_argument("--flatten_weight_dir", type=str)
+    parser.add_argument("--flatten_weight_file_name", type=str, default="generated_weights_samples.pt")
+    # parser.add_argument("--sample_model_path", type=str)
 
     args=parser.parse_args()
 
-    loaded_model = torch.load(args.flatten_weight_path)
+    loaded_model = torch.load(os.path.join(args.flatten_weight_dir, args.flatten_weight_file_name))
     flatten_weight = loaded_model['generated_weights_samples']
-    sample_model = torch.load(args.sample_model_path)
+    # sample_model = torch.load(args.sample_model_path)
+    sample_model = torch.load("/home/kctung/Projects/HyperDiffusion/logs/siren_uncond_diffusion_256/20260106-214220/sample_siren.pt")
     
     # use the first sample in the input (flatten weight)
     reshape_weight = reshape_weight_back(flatten_weight[0], sample_model['net_state_dict'])
@@ -37,4 +38,4 @@ if __name__ == "__main__":
     # save_model['light_dir_spherical'] = [[0.5, 0.5]]
     # import pdb; pdb.set_trace()
     
-    torch.save(save_model, os.path.join(args.save_path, 'sample_siren.pt'))
+    torch.save(save_model, os.path.join(args.flatten_weight_dir, 'sample_siren.pt'))
